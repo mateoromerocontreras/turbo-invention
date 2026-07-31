d7c5b8b9022edbab65cc6f260317fabba3ce2d0a
mateoromero <mateo.romero.contreras@gmail.com>
2026-07-31 12:51:04 -0300

    controller

--- backend/src/main/java/com/tailorcvpro/backend/user/controller/UserController.java
+++ backend/src/main/java/com/tailorcvpro/backend/user/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.tailorcvpro.backend.user.controller;
 
+import com.tailorcvpro.backend.user.dto.UserCreateDto;
+import com.tailorcvpro.backend.user.dto.UserResponseDto;
+import com.tailorcvpro.backend.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto dto) {
+        UserResponseDto createdUser = userService.createUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
 }
