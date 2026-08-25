c58d5176ccb40d36b5da98c2389bf9fee2426fa9
mateoromero <mateo.romero.contreras@gmail.com>
2026-08-25 12:01:58 -0300

    Add tests for user authorities and account status in CustomUserDetailsTest

--- backend/src/test/java/com/tailorcvpro/backend/shared/security/CustomUserDetailsTest.java
+++ backend/src/test/java/com/tailorcvpro/backend/shared/security/CustomUserDetailsTest.java
@@ -16,14 +16,80 @@ public class CustomUserDetailsTest {
     @Test
     @DisplayName("Should assign ROLE_ADMIN when user is superuser")
     void shouldReturnAdminAuthorityWhenSuperuserIsTrue() {
-        // GIVEN: User entity with isSuperuser = true
+        // GIVEN
         User user = new User();
         user.setId(1L);
         user.setIsSuperuser(true);
-        // WHEN: CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        // WHEN
         CustomUserDetails result = new CustomUserDetails(user);
         List<GrantedAuthority> authorities = new ArrayList<>(result.getAuthorities());
-        // THEN: Assert customUserDetails.getAuthorities() contains SimpleGrantedAuthority("ROLE_ADMIN")
+        // THEN
         assertThat(authorities).containsExactly(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Test
+    @DisplayName("When user.setIsSuperuser(false) (or null), assert that authorities contain \"ROLE_USER\"")
+    void shouldReturnUserAuthorityWhenSuperuserIsFalse() {
+        // GIVEN
+        User user = new User();
+        user.setId(1L);
+        user.setIsSuperuser(false);
+        // WHEN
+        CustomUserDetails result = new CustomUserDetails(user);
+        List<GrantedAuthority> authorities = new ArrayList<>(result.getAuthorities());
+        // THEN
+        assertThat(authorities).containsExactly(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Test
+    @DisplayName("When user.setIsActive(true) it should return isEnable() true")
+    void shouldReturnEnable() {
+        // GIVEN
+        User user = new User();
+        user.setId(1L);
+        user.setIsActive(true);
+        // WHEN
+        CustomUserDetails customerUserDetails = new CustomUserDetails(user);
+        boolean result = customerUserDetails.isEnabled();
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("When user.setIsActive(false) it should return isEnable() false")
+    void shouldReturnEnableFalse() {
+        // GIVEN
+        User user = new User();
+        user.setId(1L);
+        user.setIsActive(false);
+        // WHEN
+        CustomUserDetails customerUserDetails = new CustomUserDetails(user);
+        boolean result = customerUserDetails.isEnabled();
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("When user.setIsActive(true) it should return isCredentialsNonExpired() true")
+    void shouldReturnisCredentialsNonExpired() {
+        // GIVEN
+        User user = new User();
+        user.setId(1L);
+        user.setIsActive(true);
+        // WHEN
+        CustomUserDetails customerUserDetails = new CustomUserDetails(user);
+        boolean result = customerUserDetails.isCredentialsNonExpired();
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("When user.setIsActive(false) it should return isCredentialsNonExpired() false")
+    void shouldReturnisCredentialsNonExpiredFalse() {
+        // GIVEN
+        User user = new User();
+        user.setId(1L);
+        user.setIsActive(false);
+        // WHEN
+        CustomUserDetails customerUserDetails = new CustomUserDetails(user);
+        boolean result = customerUserDetails.isCredentialsNonExpired();
+        assertThat(result).isFalse();
     }
 }
